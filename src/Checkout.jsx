4db5d03b4c018eafeecb67d553e9ccde2e8fb456
Etienne Warneck <etienne.warneck@gmail.com>
2022-02-21 17:59:07 -0500

    Add derived states for validation and errors

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -19,42 +19,71 @@ export default function Checkout({ cart, emptyCart }) {
     const [address, setAddress] = useState(emptyAddress);
     const [status, setStatus] = useState(STATUS.IDLE)
     const [saveError, setSaveError] = useState(null)
+    const [touched, setTouched] = useState({})
+
+    //Derived state
+    const errors = getErrors(address)
+    const isValid = Object.keys(errors).length === 0;
 
     function handleChange(e) {
         e.persist()
         setAddress((currentAddr) => {
             return {
                 ...currentAddr, //set addr to a copy of current addr
-                [e.target.id]: e.target.value //use input's id to determine property to set -> the value
+                [e.target.id]: e.target.value //reference a property using a variable.
             }
         })
     }
 
     function handleBlur(event) {
-        // TODO
+        event.persist()
+        setTouched((curr) => {
+            return { ...curr, [event.target.id]: true } //new prop for field that was just touched
+        })
     }
 
     async function handleSubmit(event) {
-        event.preventDefault();
+        event.preventDefault(); //prevent the form from posting back to server. Keep validation on client side.
         setStatus(STATUS.SUBMITTING)
-        try {
-            await saveShippingAddress(address);
-            emptyCart();
-            setStatus(STATUS.COMPLETED)
-        } catch (e) {
-            setSaveError(e)
+        if (isValid) {
+            try {
+                await saveShippingAddress(address);
+                emptyCart();
+                setStatus(STATUS.COMPLETED)
+            } catch (e) {
+                setSaveError(e)
+            }
+        } else {
+            setStatus(STATUS.SUBMITTED)
         }
+    }
+
+    function getErrors(address) {
+        const result = {};
+        if (!address.city) result.city = "City is required";
+        if (!address.country) result.country = "Country is required";
+        return result;
     }
 
 
     if (saveError) throw saveError;
     if (status === STATUS.COMPLETED) {
-        return <h1>thanks for shopping</h1>
+        return <h1>Thanks for shopping!</h1>
     }
 
     return (
         <>
             <h1>Shipping Info</h1>
+            {!isValid && status === STATUS.SUBMITTED && (
+                <div role="alert"> <p> Please fix errors :
+                </p>
+                    <ul> {Object.keys(errors).map((key) => {
+                        return <li key={key} >{errors[key]}</li>
+
+                    })}</ul>
+
+                </div>
+            )}
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="city">City</label>
@@ -66,6 +95,7 @@ export default function Checkout({ cart, emptyCart }) {
                         onBlur={handleBlur}
                         onChange={handleChange}
                     />
+                    <p role="alert">{(touched.city || status === STATUS.SUBMITTED) && errors.city}</p>
                 </div>
 
                 <div>
@@ -83,6 +113,7 @@ export default function Checkout({ cart, emptyCart }) {
                         <option value="United Kingdom">United Kingdom</option>
                         <option value="USA">USA</option>
                     </select>
+                    <p role="alert">{(touched.country || status === STATUS.SUBMITTED) && errors.country}</p>
                 </div>
 
                 <div>
