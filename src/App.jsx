94af3cda122d07e0159ae1eb5af7e3c7d243c7a3
Etienne Warneck <etienne.warneck@gmail.com>
2022-03-03 18:16:35 -0500

    change checkout to a class component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Products from "./Products";
 import Detail from "./Detail";
 import Cart from "./Cart";
 import Footer from "./Footer";
-import Checkout from "./Checkout"
+import Checkout from "./Checkout.class" // class
+import { useCart } from './cartContext';
 
 // let initialCart;
 
@@ -19,11 +20,14 @@ import Checkout from "./Checkout"
 
 export default function App() {
 
+  const { dispatch } = useCart() // to pass to checkout.class
+
+  //MOVED TO CONTEXT
   // const [cart, dispatch] = useReducer(cartReducer, initialCart);
 
   // useEffect(() => localStorage.setItem("cart", JSON.stringify(cart), [cart]))
 
-
+  //MOVED TO REDUCER
   // function addToCart(id, sku) {
   //   //The Function Form provides automatically the existing state.
   //   setCart((items) => {
@@ -34,7 +38,6 @@ export default function App() {
   //     } else {
   //       //return new array, if item not in cart, append new item
   //       return [...items, { id, sku, quantity: 1 }]
-
   //     }
   //   })
   // }
@@ -61,7 +64,7 @@ export default function App() {
             <Route path="/:category" element={<Products />} />
             <Route path="/:category/:id" element={<Detail />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/checkout" element={<Checkout dispatch={dispatch} />} />
           </Routes>
         </main>
       </div>
